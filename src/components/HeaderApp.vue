143770b7f93c325fa619ea1d79e2f8c50f95c8a6
Arya Saputra <arya@xctuality.com>
2023-11-06 21:31:22 +0700

    build commit2

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="header-app px-3 d-flex justify-content-between py-10 w-100 bg-white position-fixed top-0 start-0">
+    <div class="header-app px-3 d-flex justify-content-between py-10 w-100 bg-white position-fixed top-0 start-0 align-items-center">
         <div class="w-25 back text-primary text-start h3 mb-0">
             &#60;
         </div>
         <div class="w-25 text-bold">
             Trade
         </div>
-        <div class="w-25">
-            
+        <div class="w-25 h2 mb-0 text-primary d-flex justify-content-end">
+            <div class="me-3">❑</div>
+            <div>☾</div>
         </div>
     </div>
 </template>
